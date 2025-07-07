c4b66ad8ed221567594548bd626c875b41a17508
doulos30 <doulos_21@yahoo.com>
2025-07-07 13:22:55 -0500

    changed the shaper

--- blah.py
+++ blah.py
@@ -1,2 +1,6 @@
 import numpy as np
-print(np.load("calib_data.npy").shape)
+
+calib_data = np.load("calib_data.npy")  # shape (500, 1)
+calib_data = calib_data.reshape(500, 1, 1, 1)  # shape (500, 1, 1, 1)
+np.save("calib_data.npy", calib_data)
+print("Reshaped and saved as (500, 1, 1, 1)")
